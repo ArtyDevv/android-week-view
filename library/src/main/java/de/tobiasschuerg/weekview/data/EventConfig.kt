4a88d59e949cc4c7718eca471401f358ab897930
ArtyDevv <104095151+ArtyDevv@users.noreply.github.com>
2022-04-20 19:05:20 +0100

    Update EventConfig.kt

--- library/src/main/java/de/tobiasschuerg/weekview/data/EventConfig.kt
+++ library/src/main/java/de/tobiasschuerg/weekview/data/EventConfig.kt
@@ -11,5 +11,6 @@ data class EventConfig(
     val showUpperText: Boolean = true,
     val showSubtitle: Boolean = true,
     val showLowerText: Boolean = true,
-    val showTimeEnd: Boolean = true
+    val showTimeEnd: Boolean = true,
+    val showTimeStartEnd: Boolean = true
 )
